0c6c843784a0a1cd466bef195ee187e3606dec28
engloong21 <>
2026-04-22 06:31:20 +0000

    Tuple Basics

---
+++ mastering_python_tuples/tuple_basics/main.py
@@ -0,0 +1,5 @@
+# Write your code here
+movies_list = ['2001: A Space Odyssey', 'Interstellar', 'Star Wars: Episode IV - A New Hope', 'Gravity', 'The Martian']
+space_movies = tuple(movies_list)
+# Testing
+print("Movies about space:", space_movies)
